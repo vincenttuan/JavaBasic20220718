6784006136b4f114098c7c268275a6626cb83447
段維瀚 vincent tuan <vincentjava@yahoo.com.tw>
2022-07-18 15:57:05 +0800

    Add files via upload

---
+++ javacode/Hello9.java
@@ -0,0 +1,13 @@
+// 程式的邏輯判斷
+import java.util.Scanner;
+public class Hello9 {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("請輸入分數成績: ");
+		int score = scanner.nextInt();
+		System.out.println("您所輸入的成績是: " + score);
+		System.out.println("系統判斷...");
+		// 判斷成績是否及格 ?
+		System.out.println( score >= 60 ? "及格" : "不及格" );
+	}
+}
