95ab05cd4b5ca81ce2749348d99c177232cfc37e
段維瀚 vincent tuan <vincentjava@yahoo.com.tw>
2022-07-19 15:58:32 +0800

    Add files via upload

---
+++ javacode/demo/Test13.java
@@ -0,0 +1,13 @@
+package demo;
+
+public class Test13 {
+	public static void main(String[] args) {
+		for(int i=1 ; i <= 5 ; i++) {
+
+			for(int k=1 ; k <= i ; k++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+}
