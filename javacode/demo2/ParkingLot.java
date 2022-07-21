b6f57f58ede04a215b6aed2784d47222f3cb584a
段維瀚 vincent tuan <vincentjava@yahoo.com.tw>
2022-07-21 16:26:20 +0800

    Add files via upload

---
+++ javacode/demo2/ParkingLot.java
@@ -0,0 +1,50 @@
+package demo2;
+
+import java.util.Scanner;
+
+public class ParkingLot {
+	public static void main(String[] args) {
+		int parking = 0b00000; // 0
+
+		parkinglot:  // 自訂迴圈的名字
+		do {
+			// 顯示目前最新車位狀態
+			String parkingString = String.format("%5s", Integer.toBinaryString(parking));
+			parkingString = parkingString.replace(' ', '0');
+			System.out.println("停車位: " + parkingString);
+			// 使用者操作
+			Scanner scanner = new Scanner(System.in);
+			System.out.print("請選擇: 停車(1) 取車(2) 離開(0) ==> ");
+			int choice = scanner.nextInt();
+			int car = -1;
+			switch(choice) {
+				case 1: // 停車(1)
+					System.out.print("請選位 ==> ");
+					car = scanner.nextInt();
+					car = (int)Math.pow(2, car);
+					if ((parking & car) == 0) { // 判斷是否該車位是空的
+						parking = parking + car;
+						System.out.println("停車完成!");
+					} else {
+						System.out.println("該車位不可停車!");
+					}
+					break;
+				case 2: // 取車(2)
+					System.out.print("請選位 ==> ");
+					car = scanner.nextInt();
+					car = (int)Math.pow(2, car);
+					if ((parking & car) != 0) { // 判斷是否該車位是否有車
+						parking = parking - car;
+						System.out.println("取車完成!");
+					} else {
+						System.out.println("該車位無車可取!");
+					}
+					break;
+				case 0: // 離開(0)
+					break parkinglot;
+			}
+
+		} while(true);
+
+	}
+}
