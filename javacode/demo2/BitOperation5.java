5653fb60d8c9582c441bfc420f449fd5c8885c08
段維瀚 vincent tuan <vincentjava@yahoo.com.tw>
2022-07-21 15:15:20 +0800

    Add files via upload

---
+++ javacode/demo2/BitOperation5.java
@@ -0,0 +1,22 @@
+package demo2;
+
+import java.util.Scanner;
+
+public class BitOperation5 {
+	public static void main(String[] args) {
+		int parking = 0b10110;
+		System.out.println("停車位: " + Integer.toBinaryString(parking));
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("請輸入要取車的車位號: ");
+		int car = scanner.nextInt();
+		car = (int)Math.pow(2, car);
+		// 檢查該車位是否有車 ?
+		if((parking & car) != 0) {
+			parking = parking - car;
+			System.out.println("車子順利移出");
+		} else {
+			System.out.println("該車位無車可移!");
+		}
+		System.out.println("停車位: " + Integer.toBinaryString(parking));
+	}
+}
